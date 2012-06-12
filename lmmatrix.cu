40c5b0cfc047b06532d0ae54d71936e8ac9bfcff
Matthew Simpson <themattsimpson@gmail.com>
2012-06-11 20:00:04 -0500

    Changes to improve memory utilization

--- lmmatrix.cu
+++ lmmatrix.cu
@@ -7,6 +7,9 @@
     - Currently each model, indexed by id, gets a matrix of size n*p where the 
       cols corresponding to variables NOT in model id are set to 0. Instead, each
       model should get the correct n*ncol[id] matrix with those columns omitted.
+    - Storage of each model's matrix (i.e. Xm) is not optimized, but this part will be handled
+      differently in the actual fitting algorithm
+
  */
 
 
@@ -16,22 +19,23 @@
 
 
 //Converts the model ID into a binary string of variable (column) indicators
-__device__ void modelid(int id, int p, int *var, int * ncol){
+__device__ void modelid(int id, int p, int *var, int * ncol, size_t varpitch){
   
   int i, k; //loop counters
-  int remain = id+1; //remainder - it's + 1 because model 0 has no model matrix
+  int remain = id+1; //remainder - it's + 1 because model 0 has no cols and is omitted
   int tmpcol = 0; //column counter
+  int * varrow = (int *)((char*)var + id*varpitch); //point to model id's binary rep
 
   //create the binary variable indicator and count number of columns in model matrix
   for(i=0; i<p; i++){
     k = 1 << (p - i - 1);
     if (remain >= k){
-      var[i+id*p] = 1;
+      varrow[i] = 1;
       remain = remain % k;
       tmpcol += 1;
     }
     else{
-      var[i+id*p] = 0;
+      varrow[i] = 0;
     }
   }
 
@@ -41,22 +45,26 @@ __device__ void modelid(int id, int p, int *var, int * ncol){
 
 
 //Creates the model matrix for each possible model
-__global__ void modelmatrix(int * p, int * n, int * N, float * X, float * Xm, 
-			    int * ncol, int * var){
+__global__ void modelmatrix(int p, int n, int N, float * X, size_t Xpitch,  
+			    float * Xm, int * ncol, int * var, size_t varpitch){
   int id = blockIdx.x; //model id
   int i, j; //loop counters
-  
-  if(id < *N){
+  int * varrow = (int *)((char*)var + id*varpitch); //pointer to model id's binary rep
+
+  if(id < N){
     //convert model ID (id) to binary variable list
-    modelid(id, *p, var, ncol);
+    modelid(id, p, var, ncol, varpitch);
     
     //initialize model id's model matrix
-    for(i=0; i<(*n); i++)
-      for(j=0; j< (*p); j++)
-	if( var[j + id*(*p)] == 1 )
-	  Xm[ id + (i*(*p)+j)*(*N) ] = X[j + i*(*p)];
+    for(i=0; i<n; i++){
+      float * Xrow = (float *)((char*)X + i*Xpitch); //pointer to row i of X
+      for(j=0; j<p; j++){
+    	if( varrow[j] == 1 )
+    	  Xm[id + N*(j + i*p)] = Xrow[j];
 	else
-	  Xm[ id + (i*(*p)+j)*(*N) ] = 0;
+	  Xm[id + N*(j + i*p)] = 0;
+      }
+    }
   }
 }
 
@@ -66,19 +74,20 @@ int main (void){
   int p = 3;  //# covariates including intercept, i.e. cols of full model matrix
   int N = (1 << p) - 1; //Number of possible models
   int n=5;  //# observations, i.e. rows of model matrix
-  int * dp, * dn, * dN;  //device p and device n
   int i, j, k;  //loop counters
   float * X; //full model matrix
-  float * Xm; //array of all possible model matricies
+  float * Xm; //all possible model matricies
   float * dX, * dXm; //device X and device Xm
   int * ncol; //# columns in each model's model matrix
   int * dncol; //device ncol
   int * var; //binary string indicating which variables are in which models
   int * dvar;  //device var
+  size_t Xpitch, varpitch; //pitch variables for dX and dvar
+
 
   //allocate memory for X, Xm, var, and ncol
   X = (float *) malloc(n*p*sizeof(float));
-  Xm = (float *) malloc(n*p*N*sizeof(float));
+  Xm = (float *) malloc(N*n*p*sizeof(float));
   var = (int *) malloc(N*p*sizeof(int));
   ncol = (int *) malloc(N*sizeof(int));
 
@@ -88,35 +97,36 @@ int main (void){
       X[j + i*p]=pow(i+1,j);
 
   //allocate memory on device for device variables
-  cudaMalloc( (void**)&dp, sizeof(int));
-  cudaMalloc( (void**)&dn, sizeof(int));
-  cudaMalloc( (void**)&dN, sizeof(int));
-  cudaMalloc( (void**)&dX, p*n*sizeof(float));
+  //cudaMalloc( (void**)&dX, p*n*sizeof(float));
   cudaMalloc( (void**)&dncol, N*sizeof(int));
-  cudaMalloc( (void**)&dvar, N*p*sizeof(int));
+  //cudaMalloc( (void**)&dvar, N*p*sizeof(int));
   cudaMalloc( (void**)&dXm, N*n*p*sizeof(float));
+  cudaMallocPitch( (void**)&dX, &Xpitch, p*sizeof(float), n);
+  cudaMallocPitch( (void**)&dvar, &varpitch, p*sizeof(int), N);
+  
   
   //copy device variables from host
-  cudaMemcpy( dp, &p, sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy( dn, &n, sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy( dN, &N, sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy( dX, X, p*n*sizeof(float), cudaMemcpyHostToDevice);
+  //cudaMemcpy( dX, X, p*n*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy2D(dX, Xpitch, X, p*sizeof(float), p*sizeof(float), n, 
+	       cudaMemcpyHostToDevice);
 
   //Create each model's model matrix
-  modelmatrix<<<N,1>>>(dp, dn, dN, dX, dXm, dncol, dvar);
+  modelmatrix<<<N,1>>>(p, n, N, dX, Xpitch, dXm, dncol, dvar, varpitch);
+
 
   //copy updated device variables to host
   cudaMemcpy(ncol, dncol, N*sizeof(int), cudaMemcpyDeviceToHost);
-  cudaMemcpy(var, dvar, p*N*sizeof(int), cudaMemcpyDeviceToHost);
-  cudaMemcpy(Xm, dXm, p*N*n*sizeof(float),cudaMemcpyDeviceToHost);
+  //cudaMemcpy(var, dvar, p*N*sizeof(int), cudaMemcpyDeviceToHost);
+  cudaMemcpy2D(var, p*sizeof(int), dvar, varpitch, p*sizeof(int), N, 
+	       cudaMemcpyDeviceToHost);
+  cudaMemcpy(Xm, dXm, p*N*n*sizeof(float), cudaMemcpyDeviceToHost);
+  
 
   //free memory
   cudaFree(dvar);
   cudaFree(dncol);
-  cudaFree(dp);
-  cudaFree(dn);
   cudaFree(dX);
-  cudaFree(dN);
+  cudaFree(dXm);
 
   //Print each model's model matrix
   for(k=0; k<N; k++){
@@ -128,12 +138,20 @@ int main (void){
     
     for(i=0; i<n; i++){
 	for(j=0; j<p; j++){
-	  printf("%.0f  ", Xm[k + N*(j + i*p)] );
+	  printf("%.0f  ", Xm[ k + N*(j + i*p) ] );
 	}
 	printf("\n");
       }
     printf("\n\n");
   }
+
+  //free memory allocated on host
+  free(var);
+  free(ncol);
+  free(X);
+  free(Xm);
+			     
+			     
 
   //print the full model matrix (for debugging)
   //  printf("Full Matrix X\n\n");
