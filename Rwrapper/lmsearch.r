d86abb87cd1accb75871856cad1594427da7681b
Matthew Simpson <themattsimpson@gmail.com>
2012-06-14 16:52:15 -0500

    R wrapper working, but could use some bells and whistles.

--- Rwrapper/lmsearch.r
+++ Rwrapper/lmsearch.r
@@ -2,12 +2,12 @@
 library(gputools)
 
 ##converts model id to a binary string
-modelid <-  function(id, p){
-  M <- (2^p) - 1
-  binid <- rep(0,p)
-  remain <- id
-  for(i in 1:p){
-    tmp <- 2^(p - i)
+modelid <-  function(id, k){
+  M <- 2^k
+  binid <- rep(0,k)
+  remain <- id - 1
+  for(i in 1:k){
+    tmp <- 2^(k - i)
     if(remain >= tmp){
       remain <- remain - tmp
       binid[i] <- 1
@@ -18,53 +18,76 @@ modelid <-  function(id, p){
 
 ##takes model id and full model matrix, outputs model matrix for model id
 modelmat <- function(X, binid){
-  Xm <- matrix(X[,which(binid==1)], ncol=sum(binid))
-  colnames(Xm) <- colnames(X)[which(binid==1)]
+  idx <- c(1, (which(binid==1)+1) )
+  Xm <- matrix(  X[,idx], ncol=sum(binid)+1)
+  colnames(Xm) <- colnames(X)[idx]
   return(Xm)
 }
 
+
+
+
 ##needs to be turned into a function
-X <- matrix(c(rep(1,10),rnorm(10)), ncol=2)
-colnames(X) <- c("Intercept", "x1")
-Y <- rnorm(10) + X%*%t(t(c(2.5, 1.2)))
+gpuLmsearch <- function(Y, X, g, sortby="MargLike"){
+  p <- ncol(X)
+  k <- p-1
+  n <- nrow(X)
+  M <- 2^k
+  models <- list()
+  Ynorm <- sum( ( Y-sum(Y) )^2 )
+  C <- margC(n, Ynorm)
 
-p <- ncol(X)
-n <- nrow(X)
-G <- gamma(n/2)
-M <- 2^p - 1
-models <- list()
+  ##first, center X:
+  for(i in 2:p){
+    mn <- mean(X[,i])
+    X[,i] <- X[,i] - mn
+  }
+  
+  ID <- 1:M
+  BinId <- paste(ID)
+  Aic <-  ID
+  Bic <- ID
+  MargLike <- ID
+  Vars <- paste(ID)
+  
+  for(i in ID){
+    binid <- modelid(i,k)
+    Xm <- modelmat(X, binid)
+    pm <- ncol(Xm)
+    km <- pm-1
+    ##dat <- data.frame(Y,Xm)
+    gout <- gpuLm.fit(Xm, Y)
+    ssr <- sum(gout$residuals ^ 2)
+    sighat <- ssr/gout$df.residual
+    Rsq <- 1 - ssr/Ynorm
+    gout$ID <- i
+    gout$BinID <- binid
+    models[[i]] <- gout
+    BinId[i] <- paste(binid, collapse="")
+    Aic[i] <- aic(n,pm,sighat)
+    Bic[i] <- bic(n,pm,sighat)
+    MargLike[i] <- marglik(n,km, g, Rsq, C)
+    Vars[i] <- paste(colnames(Xm),collapse=" ")
+  }
 
-ID <- 1:M
-BinId <- paste(ID)
-Aic <-  ID
-Bic <- ID
-MargLike <- ID
-Vars <- paste(ID)
+  Ar <- rank(Aic)
+  Br <- rank(Bic)
+  Mr <- rank(-MargLike)
+  
+  out <- list()
+  out$list <- data.frame(ID=ID, BinaryID=BinId, AIC=Aic, AICrank=Ar, BIC=Bic,
+                         BICrank=Br, MargLike=MargLike, MLrank=Mr, Variables=Vars)
+  out$models <- models
 
-for(i in ID){
-  binid <- modelid(i,p)
-  Xm <- modelmat(X, binid)
-  pm <- ncol(Xm)
-  ##dat <- data.frame(Y,Xm)
-  gout <- gpuLm.fit(Xm, Y)
-  sighat <- sum(gout$residuals^2)/gout$df.residual
-  gout$ID <- i
-  gout$BinID <- binid
-  models[[i]] <- gout
-  BinId[i] <- paste(binid, collapse="")
-  Aic[i] <- aic(n,pm,sighat)
-  Bic[i] <- bic(n,pm,sighat)
-  MargLike[i] <- marglik(n,p,Xm,sighat,G)
-  Vars[i] <- paste(colnames(Xm),collapse=" ")
+  if(sortby=="AIC")
+    out$list <- out$list[ order(Ar),]
+  else if(sortby=="BIC")
+    out$list <- out$list[ order(Br),]
+  else
+    out$list <- out$list[ order(Mr),]
+  
+  return(out)
 }
-
-out <- list()
-out$list <- data.frame(ID=ID, BinaryID=BinId, AIC=Aic, BIC=Bic, MargLike=MargLike,
-                       Variables=Vars)
-out$models <- models
-
-
-
 
 #note: AIC and BIC defined only up to additive constant
 aic <- function(n, p, sighat){
@@ -77,12 +100,18 @@ bic <- function(n, p, sighat){
   return(out)
 }
 
-##needs to be rewritten for g-prior
-marglik <- function(n, p, X, sighat, G){
-  ssr <- ( (n-p) * sighat )^(-n/2)
-  D <- 1/sqrt( det( t(X)%*%X ) )
-  c <- 2^(p/2)*pi^((p-n)/2)
-  out <- c*G*ssr*D
+##Formula from Liang et al 2007, pg 6, assumes constant g
+##late it might be worth implementing with priors chosen for g
+marglik <- function(n, k, g, Rsq, C){
+  out <- (1+g) ^ ( (n-1-k)/2 )
+  out <- out / (   ( 1 + g*(1-Rsq) ) ^ ( (n-1)/2 ) )
+  out <- C * out 
   return(out)
 }
 
+margC <- function(n, Ynorm){
+  Ynorm <- sqrt( Ynorm  )^(-n+1)
+  K <- gamma((n-1)/2) / pi^((n-1)/2) / sqrt(n)
+  out <- K*Ynorm
+  return(out)
+}
