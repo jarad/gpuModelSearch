d0aee1f9fee267fa8ef273c184ed43b4d231f8ac
Matthew Simpson <themattsimpson@gmail.com>
2012-06-27 03:31:39 -0500

    Added comments

--- Cwrapper/lmsearch.cu
+++ Cwrapper/lmsearch.cu
@@ -8,20 +8,24 @@
 #include "lsfit.h"
 #include "qrdecomp.h"
 
+//function prototype for the function to be called from R
 extern "C" void lmsearch(float *X, int *rows, int *cols, float *Y, int *ycols, 
 			 int *g, float *aics, float *bics, float *logmargs, 
 			 int *models, int *num_save, int *sorttype );
 
+//computes aic
 float aic(int n, int p, float sighat){
   float out = 2*(p+1)+n*log(sighat);
   return(out);
 }
 
+//computes bic
 float bic(int n, int p, float sighat){
   float out = (p+1)*log(n) + n*log(sighat);
   return(out);
 }
 
+//computes log marginal likelihood
 float logmarglike(int n, int k, int g, float Rsq){
   if(k==0){
     float out = 0;
@@ -34,12 +38,12 @@ float logmarglike(int n, int k, int g, float Rsq){
   }
 }
 
+//finds the worst largest or smallest in src and output the index and the element
 void arraysort(float *src, int *idx, float *dest, int num_elem, int max){
   float tmp = *src;
   int id = 0, i;
 
   for(i = 0; i < num_elem; i++){
-   
     if(max == 1){
       if(tmp < *(src + i) ){
 	tmp = *(src + i);
@@ -59,21 +63,22 @@ void arraysort(float *src, int *idx, float *dest, int num_elem, int max){
   *idx = id;
 }
 
+//performs model search
 void lmsearch(float *X, int *rows, int *cols, float *Y, int *ycols, int *g, 
 	      float *aics, float *bics, float *logmargs, int *models, 
 	      int *num_save, int *sorttype ){
 
   int p = *cols, k = p - 1, M = 1 << k, n = *rows;
   int id, remain, tmp, km, pm, mj;
-  int binid[k];
+  int binid[k];  //binary id of current model
   int i, j;
-  float *Xm;
+  float *Xm; //pointer to model matrix of current model
   size_t fbytes = sizeof(float), ibytes = sizeof(int), dbytes = sizeof(double);
-  float *coeffs, *resids, *effects; 
+  float *coeffs, *resids, *effects; //pointers to arrays, needed for gpuLmfit
   int *rank, *pivot; 
   double *qrAux;
-  double tol = 0.0001;
-  float score, *worstscore;
+  double tol = 0.0001; //tolerance of the fit, float only supported by gputools
+  float score, *worstscore; 
   float sighat, ynorm=0, ssr, ymn=0, Rsq;
   int *worstid;
   
@@ -97,17 +102,21 @@ void lmsearch(float *X, int *rows, int *cols, float *Y, int *ycols, int *g,
     exit(1);
   }
   
+  //calculate the mean value of the response, Y
   for(i = 0; i < n; i++)
     for(j = 0; j < *ycols; j++)
       ymn += Y[i + j * n];
   ymn /= n;
 
+  //calculate (Y-Ybar)^2, used in marginal likelihood computation
   for(i = 0; i < n; i++)
     for(j = 0; j < *ycols; j++)
       ynorm += pow( Y[i + j * n] - ymn, 2 );
 
+  //for each model
   for(id = 0; id < M; id++){
-    km = 0;
+    //find the binary id and num of covariates in current model
+    km = 0; 
     remain = id;
     for(i = 0; i < k; i++){
       tmp = 1 << (k - i - 1);
@@ -122,6 +131,7 @@ void lmsearch(float *X, int *rows, int *cols, float *Y, int *ycols, int *g,
     }
     pm = km + 1;
     
+    //allocate memory for model matrix of current model
     Xm = (float *) malloc( n * pm * fbytes );
 
     if(Xm == NULL){
@@ -129,8 +139,10 @@ void lmsearch(float *X, int *rows, int *cols, float *Y, int *ycols, int *g,
     exit(1);
   }
 
-    mj = 0;
+    mj = 0; //keeps track of column numbers of current model matrix
 
+    //copy the relevant columns of the full model matrix into the current model's
+    //model matrix. Note: R stores matrices/arrays in column major format.
     for(j = 0; j < p; j++){    
       if(j == 0 || binid[j-1] == 1){
 	memcpy(Xm + mj*n, X + j*n, fbytes*n);
@@ -141,6 +153,7 @@ void lmsearch(float *X, int *rows, int *cols, float *Y, int *ycols, int *g,
       }
     }
 
+    //allocate memory for arrays required for gpuLmfit
     coeffs = (float *) calloc(pm, fbytes);
     resids = (float *) calloc(n, fbytes);
     effects = (float *) malloc(n* (*ycols) *fbytes);
@@ -163,35 +176,40 @@ void lmsearch(float *X, int *rows, int *cols, float *Y, int *ycols, int *g,
       exit(1);
     }
     
+    //required for gpuLmfit
     memcpy(effects, Y, n* (*ycols) *fbytes);
 
-    *rank = 1;
+    *rank = 1; //initialize
 
+    //required for gpuLmfit
     pivot = (int *) malloc(pm*ibytes);
     if(pivot == NULL){
       Rprintf("\nMemory for pivot failed to allocate\n");
       exit(1);
     }
-
     for(i = 0; i < pm; i++)
       pivot[i] = i;
 
+    //fit the current model
     gpuLSFitF(Xm, n, pm, Y, *ycols, tol, coeffs, resids, effects, rank, pivot, qrAux);
 
+    //compute the current model's R squared, used in marginal likelihood computation
     ssr = 0;
     for(i=0; i<n; i++)
       ssr += resids[i]*resids[i];
     sighat = ssr / (n-pm); 
     Rsq = 1 - ssr/ynorm;
 
-    
+    //update best set of models information if applicable
     if(*sorttype == 0){
+      //save info of all models
       aics[id] = aic(n, pm, sighat);
       bics[id] = bic(n, pm, sighat);
       logmargs[id] = logmarglike(n, km, *g, Rsq);
       models[id] = id+1;
     }
     else if(*sorttype == 1){
+      //save the best num_save models according to AIC
       arraysort(aics, worstid, worstscore, *num_save, 1);
       score = aic(n, pm, sighat);
       if(score < *worstscore){
@@ -202,6 +220,7 @@ void lmsearch(float *X, int *rows, int *cols, float *Y, int *ycols, int *g,
       }
     }
     else if(*sorttype == 2){
+      //save the best num_save models according to BIC
       arraysort(bics, worstid, worstscore, *num_save, 1);
       score = bic(n, pm, sighat);
       if(score < *worstscore){
@@ -212,6 +231,7 @@ void lmsearch(float *X, int *rows, int *cols, float *Y, int *ycols, int *g,
       }
     }
     else{
+      //save the best num_save models according to Marginal Likelihood
       arraysort(logmargs, worstid, worstscore, *num_save, 0);
       score = logmarglike(n, km, *g, Rsq);
       if(score > *worstscore){
@@ -222,9 +242,8 @@ void lmsearch(float *X, int *rows, int *cols, float *Y, int *ycols, int *g,
       }
     }
 
-
-
-
+    //free memory use for storing information on current model
+    //note: only memory whose size depends on the particular model
     free(coeffs);
     free(resids);
     free(effects);
@@ -234,6 +253,7 @@ void lmsearch(float *X, int *rows, int *cols, float *Y, int *ycols, int *g,
     
   }
 
+  //free remaining memory
   free(rank);
   free(worstid);
   free(worstscore);
