14c024cad50951ab70a51b403a4ef570c588939f
Matthew Simpson <themattsimpson@gmail.com>
2012-07-18 16:24:27 -0500

    For testing modifications to all three wrappers

---
+++ timing/testing.r
@@ -0,0 +1,23 @@
+source("~/gpuModelSearch/Rwrapper/rlmsearch.r")
+source("~/gpuModelSearch/Cwrapper/clmsearch.r")
+source("~/gpuModelSearch/Csmart/cslmsearch.r")
+
+set.seed(1234)
+
+##to initialize the gpu since this takes ~10 seconds the first time
+system.time(test <- gpuLm.fit(matrix(c(1,2,6,9),2,2),matrix(c(1,3),ncol=1)))
+
+n <- 100
+k <- 10
+X <- genX(n,k)
+Y <- genY(n,k,X)
+
+rtime <- system.time(rlm <- gpuRlmsearch(X,Y))
+ctime <- system.time(clm <- gpuClmsearch(X,Y))
+cstime <- system.time(cslm <- gpuCSlmsearch(X,Y))
+
+i <- 0
+
+while(i < 1000000){
+  cslm <- gpuCSlmsearch(X,Y)
+}
