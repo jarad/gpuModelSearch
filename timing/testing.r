5558843f2583ec9cb627e0d01d7e4c35175c05bc
Matthew Simpson <themattsimpson@gmail.com>
2012-07-18 18:59:47 -0500

    Removed some unnecessary code

--- timing/testing.r
+++ timing/testing.r
@@ -7,8 +7,8 @@ set.seed(1234)
 ##to initialize the gpu since this takes ~10 seconds the first time
 system.time(test <- gpuLm.fit(matrix(c(1,2,6,9),2,2),matrix(c(1,3),ncol=1)))
 
-n <- 100
-k <- 10
+n <- 10
+k <- 2
 X <- genX(n,k)
 Y <- genY(n,k,X)
 
@@ -16,8 +16,4 @@ rtime <- system.time(rlm <- gpuRlmsearch(X,Y))
 ctime <- system.time(clm <- gpuClmsearch(X,Y))
 cstime <- system.time(cslm <- gpuCSlmsearch(X,Y))
 
-i <- 0
 
-while(i < 1000000){
-  cslm <- gpuCSlmsearch(X,Y)
-}
