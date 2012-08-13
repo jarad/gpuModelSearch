c29fd4fbea6adc556f5bf0c178159890a3bc040e
Jarad Niemi <niemi@iastate.edu>
2012-08-13 13:52:42 -0500

    added variable plot

--- timing/sanitycheck.r
+++ timing/sanitycheck.r
@@ -1,28 +1,18 @@
 ## Sanity check on Nettleton's idea:
-## Full model is y = a + xb + zg + d
-## Red model is  y = a + xb + e
-## Attempt to capture the residuals of the full model by regressing the
-## residuals of the reduced models on the new covariate, e.g. with
-## ehat = zg + d or possibly ehat = a + zg + d
 
 set.seed(154)
-dat <- data.frame(y=rnorm(5, mean=5, sd=2), x=rnorm(5, mean=5, sd=2), z=rnorm(5, mean=5, sd=2))
+n = 10
+rho = .5
+x1 = rnorm(n)
+x2 = rnorm(n,rho*x1)
+y = rnorm(n,x1+2*x2) # truth: y = x1 + 2*x2 + e
 
-xslr <- lm(y~x, data=dat)  ## simple linear regression: y = a + xb + e
-xz <- lm(y~x+z, data=dat)  ## full model
-dat$rx <- resid(xslr)
 
-xz2noint <- lm(rx~z-1, data=dat) ## ehat = zg + d
-xz2int <- lm(rx~z, data=dat)     ## ehat = a + zg + d
+y.x1 <- lm(y~x1-1)  
+x2.x1 <- lm(x2~x1-1)
 
-resids <- data.frame(xz=resid(xz), xz2noint=resid(xz2noint), xz2int=resid(xz2int))
+added.variable <- summary(lm(resid(y.x1)~resid(x2.x1)-1))
 
-## None of the resids match
-resids
+full <- summary(lm(y~x1+x2-1))
 
-SSR <- rep(0,3)
-for(i in 1:3)
-    SSR[i] <- sum(resids[,i]^2)
-
-## The sums of squared residuals also don't match
-SSR
+all.equal(coef(added.variable)[1],coef(full)[2,1])
